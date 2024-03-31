64a962c388181fb462ed0353646919d38e9c9334
Riyan <riyan04314@gmail.com>
2024-03-31 23:32:59 +0530

    3 three

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app       = express();
 
 app.use('/', (req, res, next)=>{
     console.log('-: Welcome 3000 AB:-');
-    res.send('-: Welcome 3000 AC3:-');
+    res.send('-: Welcome 3000 AC44:-');
     next();
 });
 
