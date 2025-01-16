e26c7f41fb0544b0e83598aa33c47b2dd764aeca
Prasenjit Aluni <prasenjit.aluni@gmail.com>
2025-01-16 06:42:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ console.log('\n\n-: App Started :-');
 const express   = require('express');
 const app       = express();
 
+/*
 app.use('/', (req, res, next)=>{
     console.log('-: Welcome 3000 AB:-');
     res.send('-: Welcome 3000 AC817:-');
     next();
 });
-
+*/
 // Set-ExecutionPolicy RemoteSigned
 // Set-ExecutionPolicy Unrestricted
 // Get-ExecutionPolicy
